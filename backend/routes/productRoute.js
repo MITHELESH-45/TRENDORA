3defd96cefeef8a804c4d256078f57425a17ecf0
Mithelesh-45 <mitheleshk3005@gmail.com>
2025-08-11 18:18:38 +0530

    created authentication for admin

--- backend/routes/productRoute.js
+++ backend/routes/productRoute.js
@@ -1,13 +1,14 @@
 import express from 'express';
 import ProductController from '../controllers/ProductController.js';
 import upload from '../middleware/multer.js';
+import adminauth from '../middleware/adminauth.js';
 
 const { addProduct, listProduct, removeProduct, singleProduct } = ProductController;
 
 const productRouter = express.Router();
 
 productRouter.post(
-  '/add',
+  '/add',adminauth,
   upload.fields([
     { name: 'image1', maxCount: 1 },
     { name: 'image2', maxCount: 1 },
@@ -16,7 +17,7 @@ productRouter.post(
   ]),
   addProduct
 );
-productRouter.post('/remove', removeProduct);
+productRouter.post('/remove',adminauth, removeProduct);
 productRouter.get('/list', listProduct);
 productRouter.post('/single', singleProduct);
 
